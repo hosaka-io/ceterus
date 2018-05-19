a67433f45064aa3477e6267cef7ce6d7357a7419
Brian Edwards <brian@edwardstx.us>
2018-05-19 14:55:48 -0700

    Fixed SQL

--- resources/db/migration/V4__uuid_types.sql
+++ resources/db/migration/V4__uuid_types.sql
@@ -2,7 +2,7 @@ ALTER TABLE ceterus.service_key_values
 ALTER COLUMN id TYPE uuid USING id::uuid;
 
 ALTER TABLE ceterus.service_key_values
-ALTER COLUMN service TYPE uuid USING id::uuid;
+ALTER COLUMN service TYPE uuid USING service::uuid;
 
 ALTER TABLE ceterus.service_key_values
 ALTER COLUMN id SET DEFAULT uuid_generate_v4();
